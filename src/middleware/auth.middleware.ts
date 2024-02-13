21eb2af3c79f25504bd01e459debeb90515f07f3
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-02-13 01:56:30 +0100

    Implement authentication middleware.

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,40 @@
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const protect = async (req: Request, res: Response, next: NextFunction) => {
+	let token;
+
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		try {
+			token = req.headers.authorization.split(' ')[1];
+			const decoded: any = jwt.verify(token, process.env.SECRET_KEY);
+
+			req.user = await prisma.user.findUnique({
+				where: {
+					id: decoded.id,
+				},
+				select: {
+					id: true,
+					email: true,
+				},
+			});
+
+			next();
+		} catch (error) {
+			console.error(error);
+			res.status(401).json({
+				status: 401,
+				message: 'Authentication failed. Please check your token.',
+			});
+		}
+	} else {
+		res.status(401).json({
+			status: 401,
+			message: 'Unauthorized',
+		});
+	}
+};
+
+export default protect;
